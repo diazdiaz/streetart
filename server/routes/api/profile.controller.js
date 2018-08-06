fb4a138fe9a069d80170f873cfc5a83d707d7ec3
Pablo Diaz <pdiaz.ieu2014@student.ie.edu>
2018-08-06 12:43:04 +0200

    haha

--- server/routes/api/profile.controller.js
+++ server/routes/api/profile.controller.js
@@ -14,6 +14,7 @@ router.get("/", (req, res, next) => {
 // Retrive DETAIL
 router.get("/:id", (req, res, next) => {
   User.findById(req.params.id)
+  .populate('works')
     .then(user => {
       return res.json(user)
     })
